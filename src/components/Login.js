b166a47973fb29cf51ebbd23a71296e69ea398b7
Venkata Mahesh <bhukyavenkatamahesh@gmail.com>
2023-12-17 00:48:48 +0530

    Form Validation using useRef Hook

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/Validate";
 
 const Login = () => {
   const [IsSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
 
   const toggleSignInForm = () => {
     setIsSignInForm(!IsSignInForm);
   };
 
+  const handleButtonClick = () => {
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+  };
   return (
     <div>
       <Header />
@@ -19,7 +28,12 @@ const Login = () => {
       </div>
 
       <div>
-        <form className="w-3/12 absolute p-12 bg-black bg-opacity-80 my-36 mx-auto right-0 left-0 text-white rounded-md">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          className="w-3/12 absolute p-12 bg-black bg-opacity-80 my-36 mx-auto right-0 left-0 text-white rounded-md"
+        >
           <h1 className="font-bold text-3xl py-4 m-1">
             {IsSignInForm ? "Sign In" : "Sign Up"}
           </h1>
@@ -27,20 +41,26 @@ const Login = () => {
             <input
               type="text"
               placeholder="First Name"
-              className="p-3 my-4 w-full rounded-md"
+              className="p-3 my-4 w-full rounded-md text-black"
             />
           )}
           <input
+            ref={email}
             type="text"
             placeholder="Email or Phone Number"
-            className="p-3 my-4 w-full rounded-md"
+            className="p-3 my-4 w-full rounded-md text-black"
           />
           <input
+            ref={password}
             type="text"
             placeholder="Password"
-            className="p-3 my-4 w-full rounded-md"
+            className="p-3 my-4 w-full rounded-md text-black"
           />
-          <button className="p-3 my-4 bg-red-700 w-full rounded-md">
+          <p className="text-red-500 ">{errorMessage}</p>
+          <button
+            className="p-3 my-4 bg-red-700 w-full rounded-md"
+            onClick={handleButtonClick}
+          >
             {IsSignInForm ? "Sign In" : "Sign Up"}
           </button>
           <p className="p-3 cursor-pointer" onClick={toggleSignInForm}>
